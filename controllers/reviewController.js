b073481eb5774f12db490113569de28bb3730e64
yasith wijesuriya <yasithruchiranga95@gmail.com>
2025-01-08 11:09:21 +0530

    update reviewController.js file

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,27 +1,26 @@
 import reviews from "../models/review.js";
 
-export function addReview(req,res){
-    if(req.User == null){
-        res.status(401).send({
-            message :"please login and try again",
-        })
+export function addReviews(req, res) {
+    if (req.User == null) {
+        res.status(401).json({
+                message: "please login and try again",
+            })
+        }
     return;
     }
-
-    const data =req.body;
-    data.name = req.User.firstName + " " + req.User.lastName;
+    const data = req.body;
+    data.name = req.User.firstName + " "+ req.User.lastName;
     data.email = req.User.email;
-    data.profilePicture = req.User.profilePicture;
+    data.profilePicture = req.Use.profilePicture;
 
     const newReview = new reviews(data);
     newReview.save().then(()=>{
-        res.status(201).send({
-            message :"review added successfully",
+        res.status(201).json({
+            message: "review added successfully",
         })
     }).catch(()=>{
-        res.status(500).send({
-            message :"failed to add review",
+        res.status(500).json({
+            message: "failed to add review",
         })
     })
-
-}
+    
