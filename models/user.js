9e6cb3cbc9d2288eebf26675e4493801dd34db42
yasith <yasithruchiranga95@gmail.com>
2025-08-30 01:42:12 +0530

    changed node version

--- models/user.js
+++ models/user.js
@@ -55,44 +55,4 @@ const userSchema = new mongoose.Schema({
 
     export default User;
 
-    /*
-    "email": "sriyan@gmail.com",
-    "password": "12345",
-    "role": "customer",
-    "firstName": "modaya",
-    "lastName": "yasith",
-    "address": "123 Main St, Anytown, USA",
-    "phone": "+1 555 123 4567",
-    "profilePicture": "default.jpg"
-
-
-      "email": "yasith@gmail.com",
-    "password": "12345",
-    "role": "admin",
-    "firstName": "yasith",
-    "lastName": "wijesuriya",
-    "address": "123 Main St, Anytown, USA",
-    "phone": "+1 555 123 4567",
-    "profilePicture": "default.jpg"
-
-
-     "email": "methmiEkanayaka@gmail.com",
-    "password": "12345",
-    "role": "admin",
-    "firstName": "methmi",
-    "lastName": "wijesuriya",
-    "address": "123 Main St, Anytown, USA",
-    "phone": "+1 555 123 4567",
-    "profilePicture": "default.jpg"
-
-    "email": "heshani@gmail.com",
-    "password": "123456",
-    "role": "customer",
-    "firstName": "heshani",
-    "lastName": "tharuka",
-    "address": "123 Main St, Anytown, USA",
-    "phone": "+1 555 123 4567",
-    "profilePicture": "default.jpg"
-
     
-    */
